91fc8084c360017597cf26666619f9e6ab9194dd
Jerry Lanning <lanning.j@husky.neu.edu>
2019-04-20 16:03:40 -0400

    Talk to 8080

--- src/api-content/ApiContent.js
+++ src/api-content/ApiContent.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import '../home/HomePage.css';
+import CryptoService from "../services/CryptoService";
 
 export default class ApiContent extends React.Component {
     constructor(props) {
         super(props);
+        this.cryptoService = CryptoService.getInstance();
+        this.state = {
+            crypto: []
+        }
+
+    }
+
+    componentDidMount() {
+        this.cryptoService.findAllCrypto().then(crypto => {
+            this.setState({
+                crypto: crypto
+            });
+            console.log(crypto);
+        })
     }
 
     render() {
---
+++ src/services/CryptoService.js
@@ -0,0 +1,22 @@
+let CRYPTO_API_URL =
+    'http://localhost:8080/api/crypto';
+
+export default class CryptoService{
+    static myInstance = null;
+    static getInstance() {
+        if (CryptoService.myInstance == null) {
+            CryptoService.myInstance = new CryptoService();
+        }
+        return this.myInstance
+    }
+
+    findAllCrypto = () => {
+        return fetch(CRYPTO_API_URL, {
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(response =>
+            response.json());
+    };
+}
