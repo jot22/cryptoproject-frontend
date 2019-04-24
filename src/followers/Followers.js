c7fc8f8b63d0f50a1c0191a4536c7a01e34231f5
Jerry Lanning <lanning.j@husky.neu.edu>
2019-04-23 22:26:48 -0400

    Following

--- src/followers/Followers.js
+++ src/followers/Followers.js
@@ -1,9 +1,36 @@
 import React, {Component} from 'react';
+import UserService from "../services/UserService";
+import FollowersService from "../services/FollowersService";
 
 
 export default class Followers extends Component {
     constructor(props) {
         super(props);
+        this.userService = UserService.getInstance();
+        this.follorService = FollowersService.getInstance();
+        this.state = {
+            user: {},
+            following: []
+        }
+    }
+
+    componentDidMount() {
+        let users = [];
+        this.userService.profile().then(response => {
+            this.follorService.findFollowingByUserId(response._id).then(newResponse => {
+                newResponse.following.map(user => {
+                        this.userService.findUserById(user).then(newestResponse => {
+                            users.push(newestResponse);
+                        })
+                    }
+                )
+                console.log(users);
+                this.setState({
+                    user: response,
+                    following: users
+                })
+            })
+        })
     }
 
     render() {
@@ -23,7 +50,17 @@ export default class Followers extends Component {
                         </tr>
                         </thead>
                         <tbody id={"tableBodyPort"}>
-
+                        {
+                            console.log(this.state.following)
+                            // this.state.following.map(user => {
+                            //     console.log(user);
+                            //     return (
+                            //         <tr id={"tableRows"}>
+                            //             {user.firstName}
+                            //         </tr>
+                            //         )
+                            // })
+                        }
                         </tbody>
                     </table>
                 </div>
